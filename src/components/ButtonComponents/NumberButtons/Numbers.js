0677f04ec42d1a7a473a00dbb185fe125c1dd8aa
Justine Gennaro <justine.rouzier@gmail.com>
2019-08-13 16:20:40 -0500

    Added the display and the numbers on the calculator

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import NumberButton from "./NumberButton"
 //import any components needed
 
 //Import your array data to from the provided data file
@@ -12,6 +13,8 @@ const Numbers = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {numberState.map( num => <NumberButton text={num}/>)}
+        
     </div>
   );
 };
